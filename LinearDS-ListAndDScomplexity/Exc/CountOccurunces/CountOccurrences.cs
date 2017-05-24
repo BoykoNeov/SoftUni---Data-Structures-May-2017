894b7ad5b98e80515519930090b2b21e5c816baa
BoykoNeov <boikoneov@gmail.com>
2017-05-24 20:38:33 +0300

    :herb: Finished task "Count Occurrunces"

---
+++ LinearDS-ListAndDScomplexity/Exc/CountOccurunces/CountOccurrences.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// Write a program that finds in given array of integers how many times each of them occurs. 
+/// The input sequence holds numbers in range [0…1000]. The output should hold all numbers that 
+/// occur at least once along with their number of occurrences. 
+/// </summary>
+namespace CountOccurrences
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountOccurrences
+    {
+        public static void Main()
+        {
+
+
+            string[] inputs = Console.ReadLine().Split();
+            Dictionary<int, int> occurences = new Dictionary<int, int>();
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int currentName = int.Parse(inputs[i]);
+                if (!occurences.ContainsKey(currentName))
+                {
+                    occurences.Add(currentName, 0);
+                }
+
+                occurences[currentName]++;
+            }
+
+            foreach (var kvp in occurences.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value} times");
+            }
+        }
+    }
+}
