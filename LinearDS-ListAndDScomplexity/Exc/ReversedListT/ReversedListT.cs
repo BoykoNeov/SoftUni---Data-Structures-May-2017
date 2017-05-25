6c9a3e799e238e80c8f6a4d554cec1f59c9c83cc
BoykoNeov <boikoneov@gmail.com>
2017-05-25 12:21:55 +0300

    :foggy: Started work on "Reversed List" task

---
+++ LinearDS-ListAndDScomplexity/Exc/ReversedListT/ReversedListT.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedListT
+{
+    public class ReversedList<T>
+    {
+        private const int InitialCapacity = 2;
+
+        private T[] items;
+        public ReversedList()
+        {
+            this.items = new T[InitialCapacity];
+        }
+
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return this.items[index];
+            }
+
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                this.items[index] = value;
+            }
+        }
+    }
+}
