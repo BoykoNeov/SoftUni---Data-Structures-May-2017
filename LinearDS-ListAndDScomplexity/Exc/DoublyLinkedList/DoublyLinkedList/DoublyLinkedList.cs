805d6a95965ed999df8370f4b4759f705ab5a438
BoykoNeov <boikoneov@gmail.com>
2017-05-27 00:01:05 +0300

    :rose: Finished task "Doubly Linked List"

---
+++ LinearDS-ListAndDScomplexity/Exc/DoublyLinkedList/DoublyLinkedList/DoublyLinkedList.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoublyLinkedList<T> : IEnumerable<T>
+{
+    private class ListNode<T>
+    {
+        public T Value { get; private set; }
+
+        public ListNode<T> NextNode { get; set; }
+
+        public ListNode<T> PrevNode { get; set; }
+
+        public ListNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public T LastNode { get; private set; }
+    }
+
+    private ListNode<T> head;
+    private ListNode<T> tail;
+
+    public int Count { get; private set; }
+
+    public void AddFirst(T element)
+    {
+        if (this.Count == 0)
+        {
+            this.head = this.tail = new ListNode<T>(element);
+        }
+        else
+        {
+            var newHead = new ListNode<T>(element)
+            {
+                NextNode = this.head
+            };
+            this.head.PrevNode = newHead;
+            this.head = newHead;
+        }
+
+        this.Count++;
+    }
+
+    public void AddLast(T element)
+    {
+        if (this.Count == 0)
+        {
+            this.head = this.tail = new ListNode<T>(element);
+        }
+        else
+        {
+            var newTail = new ListNode<T>(element)
+            {
+                PrevNode = this.tail
+            };
+            this.tail.NextNode = newTail;
+            this.tail = newTail;
+        }
+
+        this.Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("List empty");
+        }
+
+        T firstElement = this.head.Value;
+        this.head = this.head.NextNode;
+        if (this.head != null)
+        {
+            this.head.PrevNode = null;
+        }
+        else
+        {
+            this.tail = null;
+        }
+
+        this.Count--;
+        return firstElement;
+    }
+
+    public T RemoveLast()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("List empty");
+        }
+
+        T lastElement = this.tail.Value;
+        this.tail = this.tail.PrevNode;
+        if (this.tail != null)
+        {
+            this.tail.NextNode = null;
+        }
+        else
+        {
+            this.head = null;
+        }
+
+        this.Count--;
+        return lastElement;
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        DoublyLinkedList<T>.ListNode<T> currentNode = this.head;
+        while (currentNode != null)
+        {
+            action(currentNode.Value);
+            currentNode = currentNode.NextNode;
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        DoublyLinkedList<T>.ListNode<T> curretNode = this.head;
+        while (curretNode != null)
+        {
+            yield return curretNode.Value;
+            curretNode = curretNode.NextNode;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    public T[] ToArray()
+    {
+        T[] arr = new T[this.Count];
+        int index = 0;
+        DoublyLinkedList<T>.ListNode<T> currentNode = this.head;
+
+        while (currentNode != null)
+        {
+            arr[index++] = currentNode.Value;
+            currentNode = currentNode.NextNode;
+        }
+
+        return arr;
+    }
+}
+
+
+class Example
+{
+    static void Main()
+    {
+        var list = new DoublyLinkedList<int>();
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+
+        list.AddLast(5);
+        list.AddFirst(3);
+        list.AddFirst(2);
+        list.AddLast(10);
+        Console.WriteLine("Count = {0}", list.Count);
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+
+        list.RemoveFirst();
+        list.RemoveLast();
+        list.RemoveFirst();
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+
+        list.RemoveLast();
+
+        list.ForEach(Console.WriteLine);
+        Console.WriteLine("--------------------");
+    }
+}
