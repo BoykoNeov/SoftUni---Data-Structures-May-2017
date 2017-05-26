42e9e533d3cea95715e6aa2f44cd9a6ad75c6629
BoykoNeov <boikoneov@gmail.com>
2017-05-26 14:17:19 +0300

    :japanese_ogre: Finished task "Distance In Labyrinth"

---
+++ LinearDS-ListAndDScomplexity/Exc/DistanceInLabyrinth/DistanceInLabyrinth.cs
@@ -0,0 +1,97 @@
+﻿/// <summary>
+/// We are given a labyrinth of size N x N. Some of its cells are empty (0) and some are full (x).
+/// We can move from an empty cell to another empty cell if they share common wall. Given a starting position (*)
+/// calculate and fill in the array the minimal distance from this 
+/// position to any other cell in the array. Use "u" for all unreachable cells.
+/// </summary>
+using System;
+using System.Collections.Generic;
+
+public class DistanceInLabyrinth
+{
+    public static void Main()
+    {
+        int labyrinthSize = int.Parse(Console.ReadLine());
+        string[,] labyrinth = new string[labyrinthSize, labyrinthSize];
+
+        Queue<Point> traverseOrder = new Queue<Point>();
+
+        for (int y = 0; y < labyrinthSize; y++)
+        {
+            string labyrinthRow = Console.ReadLine();
+
+            for (int x = 0; x < labyrinthSize; x++)
+            {
+                labyrinth[y, x] = labyrinthRow[x].ToString();
+                if (labyrinth[y, x] == "*")
+                {
+                    Point start = new Point(y, x, 0);
+                    traverseOrder.Enqueue(start);
+                }
+            }
+        }
+
+        while (traverseOrder.Count != 0)
+        {
+            Point currentPoint = traverseOrder.Dequeue();
+            int y = currentPoint.Y;
+            int x = currentPoint.X;
+            int currentPointDistance = currentPoint.PreviousPointDistance + 1;
+
+            int currentPointValue = 0;
+
+            if (labyrinth[y ,x] != "*")
+            {
+                if (int.TryParse(labyrinth[y, x], out currentPointValue) && currentPointValue == 0)
+                {
+                    labyrinth[y, x] = currentPointDistance.ToString();
+                }
+                else
+                {
+                    continue;
+                }
+            }
+            else
+            {
+                currentPointDistance--;
+            }
+
+            if (x + 1 < labyrinthSize)
+            {
+                traverseOrder.Enqueue(new Point(y, x + 1, currentPointDistance));
+            }
+
+            if (x - 1 >= 0)
+            {
+                traverseOrder.Enqueue(new Point(y, x - 1, currentPointDistance));
+            }
+
+            if (y + 1 < labyrinthSize)
+            {
+                traverseOrder.Enqueue(new Point(y + 1, x, currentPointDistance));
+            }
+
+            if (y - 1 >= 0)
+            {
+                traverseOrder.Enqueue(new Point(y - 1, x, currentPointDistance));
+            }
+        }
+
+        for (int y = 0; y < labyrinthSize; y++)
+        {
+            for (int x = 0; x < labyrinthSize; x++)
+            {
+                if (labyrinth[y, x] == "0")
+                {
+                    labyrinth[y, x] = "u";
+                }
+
+                Console.Write(labyrinth[y, x]);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+    }
+}
---
+++ LinearDS-ListAndDScomplexity/Exc/DistanceInLabyrinth/Point.cs
@@ -0,0 +1,38 @@
+﻿/// <summary>
+/// class for holding point coordinates
+/// </summary>
+public class Point
+{
+    public int Y { get; private set; }
+    public int X { get; private set; }
+    public int PreviousPointDistance { get; private set; }
+
+   public Point(int y, int x, int previousDistance)
+    {
+        this.Y = y;
+        this.X = x;
+        this.PreviousPointDistance = previousDistance;
+    }
+
+    public override bool Equals(object obj)
+    {
+        var other = obj as Point;
+        if (other == null)
+        {
+            return false;
+        }
+        return this.X == other.X && this.Y == other.Y;
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked // Overflow is fine, just wrap
+        {
+            int hash = 17;
+            // Suitable nullity checks etc, of course :)
+            hash = hash * 23 + Y.GetHashCode();
+            hash = hash * 23 + X.GetHashCode();
+            return hash;
+        }
+    }
+}
