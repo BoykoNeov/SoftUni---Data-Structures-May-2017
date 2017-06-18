46f3b37f5a4345b6fafd624f71cb4fe3376b1ccc
BoykoNeov <boikoneov@gmail.com>
2017-06-18 11:18:16 +0300

    :construction_worker: Work on "Text Editor"

--- RopeAndTrie-Excercise/Launcher.cs
+++ RopeAndTrie-Excercise/Launcher.cs
@@ -1,26 +1,49 @@
 ﻿using System;
-
+using Wintellect.PowerCollections;
 namespace TrieRope
 {
-    class Launcher
+    public class Launcher
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            Trie<string> proba = new Trie<string>();
+            proba.Insert("bace", "123");
+            Console.WriteLine(proba.GetValue("bace"));
+            proba.Insert("bace", "653");
+            Console.WriteLine(proba.GetValue("bace"));
+
+            BigList<string> probaBigList = new BigList<string>();
+            probaBigList.Insert(0, "erdfdgdfdg");
+            Console.WriteLine(string.Join("", probaBigList));
+
             StringEditor stringEditor = new StringEditor();
 
             stringEditor.Login("pesho");
-            stringEditor.Undo("pesho");
+            //stringEditor.Login("pesho");
 
-
-            stringEditor.Prepend("pesho", "stringexample");
-            stringEditor.Delete("pesho", 3, 6);
-            stringEditor.Undo("pesho");
-
-            stringEditor.Substring("pesho", 0, 3);
-            stringEditor.Undo("pesho");
-
-
+            stringEditor.Prepend("pesho", "1234567890");
+            stringEditor.Prepend("pesho", "ABCDEFGHIG".ToLower());
+            stringEditor.Prepend("pesho", "1234567890");
+            stringEditor.Substring("pesho", 1, 2);
             Console.WriteLine(stringEditor.Print("pesho"));
+
+            //stringEditor.Logout("pesho");
+            //stringEditor.Undo("pesho");
+            //stringEditor.Undo("pesho");
+            //stringEditor.Undo("pesho");
+            //stringEditor.Undo("pesho");
+
+
+
+            //stringEditor.Prepend("pesho", "stringexample");
+            //stringEditor.Delete("pesho", 3, 6);
+            //stringEditor.Undo("pesho");
+
+            //stringEditor.Substring("pesho", 0, 3);
+            //stringEditor.Undo("pesho");
+
+
+            //Console.WriteLine(stringEditor.Print("pesho"));
         }
     }
 }
--- RopeAndTrie-Excercise/TextEditor.cs
+++ RopeAndTrie-Excercise/TextEditor.cs
@@ -2,73 +2,158 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Wintellect.PowerCollections;
 
 namespace TrieRope
 {
     class StringEditor : ITextEditor
     {
+        public Trie<string> loggedUsers;
+        public Dictionary<string, Stack<BigList<char>>> userStrings;
 
-         
         public StringEditor()
         {
-
+            loggedUsers = new Trie<string>();
+            userStrings = new Dictionary<string, Stack<BigList<char>>>();
         }
 
         public void Login(string username)
         {
-            throw new NotImplementedException();
+            loggedUsers.Insert(username, string.Empty);
+            userStrings[username] = new Stack<BigList<char>>();
         }
 
         public void Logout(string username)
         {
-            throw new NotImplementedException();
+            loggedUsers.Delete(username);
+            userStrings.Remove(username);
         }
 
         public string Print(string username)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username))
+            {
+                return null;
+            }
+
+            string currentString = string.Empty;
+            currentString = string.Join("", userStrings[username].Peek());
+            return currentString;
         }
 
         public void Prepend(string username, string str)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username))
+            {
+                return;
+            }
+
+            BigList<char> newString = new BigList<char>();
+
+            if (userStrings[username].Count > 0)
+            {
+                newString = userStrings[username].Peek().Clone();
+            }
+
+            newString.AddRangeToFront(str);
+            userStrings[username].Push(newString);
         }
 
         public void Clear(string username)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username))
+            {
+                return;
+            }
+
+            BigList<char> newString = new BigList<char>();
+            userStrings[username].Push(newString);
         }
 
         public void Delete(string username, int startIndex, int length)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username) || userStrings[username].Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                BigList<char> newString = new BigList<char>();
+                newString = userStrings[username].Peek().Clone();
+                newString.RemoveRange(startIndex, length);
+                userStrings[username].Push(newString);
+            }
+            catch
+            {
+            }
+
         }
 
         public void Insert(string username, int index, string str)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username) || userStrings[username].Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                BigList<char> newString = new BigList<char>();
+                newString = userStrings[username].Peek().Clone();
+                newString.InsertRange(index, str);
+                userStrings[username].Push(newString);
+            }
+            catch
+            {
+            }
         }
 
         public int Length(string username)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username) || userStrings[username].Count == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                int result = userStrings[username].Peek().Count;
+                return result;
+            }
         }
 
         public void Substring(string username, int startIndex, int length)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username) || userStrings[username].Count == 0)
+            {
+                return;
+            }
+
+            try
+            {
+                BigList<char> newString = new BigList<char>();
+                newString.AddRangeToFront(userStrings[username].Peek().Range(startIndex, length));
+                userStrings[username].Push(newString);
+            }
+            catch
+            {
+            }
         }
 
         public void Undo(string username)
         {
-            throw new NotImplementedException();
+            if (!userStrings.ContainsKey(username) || userStrings[username].Count <= 1)
+            {
+                return;
+            }
+
+            userStrings[username].Pop();
+
         }
 
         public IEnumerable<string> Users(string prefix = "")
         {
-            throw new NotImplementedException();
+           return loggedUsers.GetByPrefix(prefix);
         }
     }
 }
